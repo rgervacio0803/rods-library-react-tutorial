85bfc7dd02f94aa80a6a8df75ddbe29fdfcc791c
rgervacio0803 <ogturtel@yahoo.com>
2025-07-24 15:02:08 -0700

    7th commit

--- src/pages/Bookinfo.jsx
+++ src/pages/Bookinfo.jsx
@@ -1,20 +1,29 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Bookinfo({ books }) {
   return (
-<div id="books__body">
-   <main id="books__main">
-    <div className="books__container">
-      <div className="row">
-       <div className="book__selected--top">
-        <Link to="/books" className="book__link"></Link>
-        <FontAwesomeIcon icon="arrow-left" />
-        </div>     
-    </div>  
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                </figure>  
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-    </main> 
-</div>
-  )
+  );
 }
